05c410e006e67e00511c25d404eed74c3ce162c0
yhs97 <sarvaiyayash@gmail.com>
2021-02-25 16:40:06 -0700

    Initial springboot app

---
+++ src/main/java/com/example/ec2autoscaling/Ec2AutoscalingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2autoscaling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2AutoscalingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2AutoscalingApplication.class, args);
+	}
+
+}
